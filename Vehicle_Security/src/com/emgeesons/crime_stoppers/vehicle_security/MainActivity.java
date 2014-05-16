51d336fb3befe94608792e3cf19aaaf6512b8d54
Gaurav <gaurav@emgeesons.com>
2014-05-16 10:35:06 +0530

    Initial Commit

---
+++ Vehicle_Security/src/com/emgeesons/crime_stoppers/vehicle_security/MainActivity.java
@@ -0,0 +1,25 @@
+package com.emgeesons.crime_stoppers.vehicle_security;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		TextView as ;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
